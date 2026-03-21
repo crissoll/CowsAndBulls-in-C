0105b209fef6fc98edbd961054de035d7b1555a2
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-03-21 20:49:52 +0100

    FIle Cleansing
    -Removed IT word list (after noticing it had bad quality)
    -Removed extract_5_letters_words.c, that wasn't really used in the program

--- src/util/cab_consts.h
+++ src/util/cab_consts.h
@@ -11,7 +11,7 @@
 /* string literals for the word‑list filenames; using macros avoids
    multiple‑definition linker errors that occur when a header defines
    non‑`static` objects. */
-# define IT_FILE_NAME "data/words/5_letters_it_words.txt"
+
 # define EN_FILE_NAME "data/words/5_letters_en_words.txt"
 
 
