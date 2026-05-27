89c8d81f4d3bacc60a533745167fb41128c20e01
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-05-27 17:17:56 +0200

    minor used_vocabolary refactoring

--- src/game/cab_used_vocabolary.c
+++ src/game/cab_used_vocabolary.c
@@ -6,8 +6,17 @@
 
 static Vocabolary vocab_storage;
 
+
 bool word_is_in_used_vocabolary(Word word){
     return vocabolary__contains_word(&vocab_storage, word);
+}
+
+
+void load_vocabolary(void){
+    vocabolary__init_from_file(
+        &vocab_storage,
+        get_vocabolary_file_path()
+    );
 }
 
 
@@ -19,18 +28,12 @@ Word get_word_from_used_vocabolary(size_t index){
     return vocab_storage.words[index];
 }
 
+
 size_t get_used_vocabolary_size(){
     return vocab_storage.size;
 }
 
-Vocabolary get_used_vocabolary(){
+
+Vocabolary get_used_vocabolary(){  
     return vocab_storage;
-}
-
-
-void load_vocabolary(void){
-    vocabolary__init_from_file(
-        &vocab_storage,
-        get_vocabolary_file_path()
-    );
 }
