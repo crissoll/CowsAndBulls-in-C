35c9e60f9ea2ee308668eebdc7084b0127228e6a
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-07-06 12:53:08 +0200

    minor include cleansing

--- src/cmd/cmd_try_word.h
+++ src/cmd/cmd_try_word.h
@@ -1,7 +1,6 @@
 #ifndef CMD_TRY
 #define CMD_TRY
 
-#include <stdbool.h>
 #include <stddef.h>
 
 void cmd__try_word_from_args(size_t token_count, const char *tokens[]);
--- src/cmd_parser/cmd.c
+++ src/cmd_parser/cmd.c
@@ -1,6 +1,5 @@
 #include <stdbool.h>
 #include <stddef.h>
-#include <string.h>
 
 #include "cmd_tree.h"
 
