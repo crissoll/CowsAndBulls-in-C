ced2b8eecd36fbb35db613e6ed62d99cadb94686
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-07-08 01:34:10 +0200

    created input_test utility module

---
+++ apps/utils/input_test.c
@@ -0,0 +1,110 @@
+
+
+#include <malloc.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "cab_api.h"
+
+#include "input_test.h"
+
+
+InputTest get_empty_test() {
+    return (InputTest){
+        .count = 0,
+        .inputs = NULL,
+    };
+}
+
+void add_input_line_to_test(InputTest* test, const char* input_line) {
+    test->inputs =
+        realloc(test->inputs, (test->count + 1) * sizeof(const char*));
+
+    char* new_string = malloc(strlen(input_line) + 1);
+    strcpy(new_string, input_line);
+
+    test->inputs[test->count] = new_string;
+    test->count++;
+}
+
+void add_test_to_test_set(InputTestSet* test_set, InputTest test) {
+
+    test_set->tests =
+        realloc(test_set->tests, (test_set->count + 1) * sizeof(InputTest));
+
+    if (test_set->tests == NULL) {
+        return;
+    }
+
+    test_set->tests[test_set->count] = test;
+    (test_set->count)++;
+}
+
+InputTestSet load_test_set_from_file(const char* file_name) {
+
+    InputTestSet test_set = (InputTestSet){
+        .tests = NULL,
+        .count = 0,
+    };
+
+    FILE* fp = fopen(file_name, "r");
+
+    if (fp == NULL) {
+        printf("Error: Could not open %s\n", file_name);
+        return test_set;
+    }
+
+    char buffer[100];
+
+    InputTest cur_test = get_empty_test();
+
+    while (fgets(buffer, 100, fp) != NULL) {
+        buffer[strcspn(buffer, "\r\n")] = 0;
+
+        if (strcmp(buffer, "") == 0) {
+            if (cur_test.count > 0) {
+                add_test_to_test_set(&test_set, cur_test);
+                cur_test = get_empty_test();
+            }
+        } else {
+            add_input_line_to_test(&cur_test, buffer);
+        }
+    }
+
+    if (cur_test.count > 0) {
+        add_test_to_test_set(&test_set, cur_test);
+    }
+
+    add_test_to_test_set(&test_set, get_empty_test());
+
+    fclose(fp);
+    return test_set;
+}
+
+
+void play_test_set(const char* file_name, TurnFunction turn_func) {
+    InputTestSet test_set = load_test_set_from_file(file_name);
+    if (test_set.count == 0) {
+        printf("no tests found\n");
+        return;
+    }
+    for (size_t i = 0; test_set.tests[i].inputs != NULL; i++) {
+        printf("\n\n============== test %zu: ==============\n", i);
+        play_game_test(test_set.tests[i], turn_func);
+    }
+}
+
+
+void play_game_test(InputTest test, TurnFunction turn_function) {
+    setup_game();
+    start_new_game();
+
+    for (size_t i = 0; i < test.count && !is_game_ended(); i++) {
+        printf("\n====[INPUT %zu] \"%s\" ====\n", i, test.inputs[i]);
+        turn_function(test.inputs[i]);
+    }
+    if (is_game_ended()) {
+        printf("Won in %zu attempts", get_attempt_number());
+    }
+    shutdown_game();
+}
