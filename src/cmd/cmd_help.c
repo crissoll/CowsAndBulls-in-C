294f4b3d10958a75c1f34077b0a1f37f23f224b1
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-07-07 22:56:36 +0200

    removed direct heap string pointers in message and output calls

--- src/cmd/cmd_help.c
+++ src/cmd/cmd_help.c
@@ -6,7 +6,6 @@
 
 #include "cab_output.h"
 
-
 void print_help_text(const char* command_name) {
     const CommandSpec* candidate_spec = get_cmd_tree_root()->args;
     while (candidate_spec->name != NULL) {
@@ -15,14 +14,13 @@ void print_help_text(const char* command_name) {
             continue;
         }
         if (strcmp(candidate_spec->name, command_name) == 0) {
-            message(OT_HELP, candidate_spec->help_text);
+            message(OT_HELP, "%s", candidate_spec->help_text);
             return;
         }
         candidate_spec++;
     }
     message(OT_ALERT, "command not found!\n");
 }
-
 
 void print_help_text_from_tokens(size_t token_count, const char* tokens[]) {
     if (token_count > 1) {
@@ -39,7 +37,7 @@ void print_whole_help_text() {
         if (!(*candidate_spec->allowed)) {
             continue;
         }
-        output(candidate_spec->help_text);
+        output("%s", candidate_spec->help_text);
         candidate_spec++;
     }
     end_message();
