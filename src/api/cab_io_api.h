7aefc71f2f45a674fc1e5ffcf0321360b9d58878
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-07-08 10:38:19 +0200

    minor io_api header aesthetic refactoring

--- src/api/cab_io_api.h
+++ src/api/cab_io_api.h
@@ -7,13 +7,17 @@
 #include "cab_io_consts.h"
 
 
-InputSuccess input(char *input_string);
-char *get_output(void);
-
 void io__setup(void); // must be called before using io functions
 void io__shutdown(void); // must be called at the end of execution
 
-char** get_messages_with_tag(OutputTags tag, size_t* message_count);
+
+InputSuccess input(char *input_string);
+
+char *get_output(void);
+
 void update_output_messages();
+
+char** get_messages_with_tag(OutputTags tag, size_t* message_count);
+
 
 #endif
