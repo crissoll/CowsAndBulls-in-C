7272a2996b16b0bd0e0c3cc4e471d84a73116156
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-07-09 19:51:03 +0200

    added functions to cab_session_api

--- src/api/cab_session_api.h
+++ src/api/cab_session_api.h
@@ -1,12 +1,17 @@
 #ifndef CAB_SESS_API
 #define CAB_SESS_API
 
-#include "stdbool.h"
+#include <stdbool.h>
+#include <stddef.h>
 
 void reset_game_vars();
 
 bool prompt_to_load_game();
 
 void process_turn();
+
+bool is_game_ended();
+
+size_t get_attempt_number();
 
 #endif
