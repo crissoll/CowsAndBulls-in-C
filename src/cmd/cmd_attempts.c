56d68e696362382c12b86b469b318bcd2ee679b5
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-07-12 16:37:46 +0200

    fixed leftover typed cmd function

--- src/cmd/cmd_attempts.c
+++ src/cmd/cmd_attempts.c
@@ -9,21 +9,21 @@
 #include "word.h"
 
 
-bool compare_attempts_to_first_token(size_t token_count, const char* tokens[]) {
+void compare_attempts_to_first_token(size_t token_count, const char* tokens[]) {
     if (token_count > 1) {
         message(OT_INPUT_ERROR,
                 "too many arguments, only first will be used\n");
     }
 
     if (!can_string_be_word(tokens[0])) {
-        return false;
+        return;
     }
 
     Word candidate_word = word__new(tokens[0]);
     if (!word_is_in_used_vocabulary(candidate_word)) {
         message(OT_ALERT, "word not contained in vocabulary\n");
-        return false;
+        return;
     }
     compare_attempts_to_word(candidate_word);
-    return true;
+    return;
 }
