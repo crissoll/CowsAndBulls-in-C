d36cf66c313fbf3a3b7c12e7ffb4e9eb493ee5b7
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-07-18 17:27:53 +0200

    better OUTPUT_TAG_NAMES array in cab_io_tag_names

--- src/api/cab_io_tag_names.h
+++ src/api/cab_io_tag_names.h
@@ -1,21 +1,22 @@
 #ifndef CAB_IO_TAG_NAMES
 #define CAB_IO_TAG_NAMES
 
+#include "cab_io_consts.h"
 
 // transcription of the names of OutputTags values
 // can be used for debugging purposes
-const char* OUTPUT_TAG_NAMES[] = {
-    "NONE",
-    "USER",
-    "GUESS_RESULT",
-    "ALERT",
-    "WARNING",
-    "INPUT_ERROR",
-    "ATTEMPTS",
-    "LIST",
-    "HISTORY",
-    "HELP",
-    "WORD_COUNT",
+const char* OUTPUT_TAG_NAMES[OT_END] = {
+    [OT_NONE] = "NONE",
+    [OT_USER] = "USER",
+    [OT_GUESS_RESULT] = "GUESS_RESULT",
+    [OT_ALERT] = "ALERT",
+    [OT_WARNING] = "WARNING",
+    [OT_INPUT_ERROR] = "INPUT_ERROR",
+    [OT_ATTEMPTS] = "ATTEMPTS",
+    [OT_LIST] = "LIST",
+    [OT_FILTER] = "FILTER",
+    [OT_HELP] = "HELP",
+    [OT_WORD_COUNT] = "WORD_COUNT",
 };
 
 #endif
