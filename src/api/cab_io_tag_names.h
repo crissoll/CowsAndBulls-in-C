58ca9b21f571238aab7e2594b94ed9eb7659e71e
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-07-18 18:31:22 +0200

    fixed output_tag_names init bug

--- src/api/cab_io_tag_names.h
+++ src/api/cab_io_tag_names.h
@@ -3,20 +3,22 @@
 
 #include "cab_io_consts.h"
 
+#define LOG2(x) ((sizeof(unsigned) * 8) - __builtin_clz(x))
+
 // transcription of the names of OutputTags values
 // can be used for debugging purposes
-const char* OUTPUT_TAG_NAMES[OT_END] = {
+const char* OUTPUT_TAG_NAMES[LOG2(OT_END - 1) + 1] = {
     [OT_NONE] = "NONE",
-    [OT_USER] = "USER",
-    [OT_GUESS_RESULT] = "GUESS_RESULT",
-    [OT_ALERT] = "ALERT",
-    [OT_WARNING] = "WARNING",
-    [OT_INPUT_ERROR] = "INPUT_ERROR",
-    [OT_ATTEMPTS] = "ATTEMPTS",
-    [OT_LIST] = "LIST",
-    [OT_FILTER] = "FILTER",
-    [OT_HELP] = "HELP",
-    [OT_WORD_COUNT] = "WORD_COUNT",
+    [LOG2(OT_USER)] = "USER",
+    [LOG2(OT_GUESS_RESULT)] = "GUESS_RESULT",
+    [LOG2(OT_ALERT)] = "ALERT",
+    [LOG2(OT_WARNING)] = "WARNING",
+    [LOG2(OT_INPUT_ERROR)] = "INPUT_ERROR",
+    [LOG2(OT_ATTEMPTS)] = "ATTEMPTS",
+    [LOG2(OT_LIST)] = "LIST",
+    [LOG2(OT_FILTER)] = "FILTER",
+    [LOG2(OT_HELP)] = "HELP",
+    [LOG2(OT_WORD_COUNT)] = "WORD_COUNT",
 };
 
 #endif
