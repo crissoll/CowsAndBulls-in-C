bd44be5c19dc80832b67beb082c8b97beeafb97f
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-08-31 23:22:56 +0200

    solved minor bug
    -last attempt and attempts count weren't correctly displayed

--- src/game/cab_core.c
+++ src/game/cab_core.c
@@ -23,6 +23,8 @@ void play_word(Word word) {
 
     GuessResult result = compare_with_secret_word(word);
 
+    add_attempt(word, result);
+
     if (result.bulls >= get_word_len()) {
         message(OT_USER,
                 "Congratulations, you found the word in %zu attempts!\n",
@@ -30,11 +32,10 @@ void play_word(Word word) {
         secret_word_found = true;
         return;
     }
+
     start_message(OT_GUESS_RESULT);
     guess_result__output(result);
     end_message();
-
-    add_attempt(word, result);
 }
 
 bool is_secret_word_found() {
