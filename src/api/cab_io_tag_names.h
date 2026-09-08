d060e00fe664465da8574d2a38e78d82f9f1424e
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-09-09 01:27:00 +0200

    Update cab_io_tag_names.h
    added MSVC support, added tag names iterator

--- src/api/cab_io_tag_names.h
+++ src/api/cab_io_tag_names.h
@@ -1,9 +1,26 @@
 #ifndef CAB_IO_TAG_NAMES
 #define CAB_IO_TAG_NAMES
 
+#include <stddef.h>
 #include "cab_io_consts.h"
 
-#define LOG2(x) ((sizeof(unsigned) * 8) - __builtin_clz(x))
+#if defined(_MSC_VER) && !defined(__clang__)
+    #include <intrin.h>
+static inline unsigned int cab_log2_1based(unsigned int x) {
+    unsigned long index;
+    if (_BitScanReverse(&index, x)) {
+        return (unsigned int)(index + 1);
+    }
+    return 0;
+}
+    #define LOG2(x) cab_log2_1based(x)
+#else
+    #define LOG2(x) \
+        ((x) == 0   \
+             ? 0    \
+             : (unsigned int)((sizeof(unsigned int) * 8) - __builtin_clz(x)))
+#endif
+
 
 // transcription of the names of OutputTags values
 // can be used for debugging purposes
@@ -23,5 +40,17 @@ static const char* const CAB_OUTPUT_TAG_NAMES[LOG2(OT_END - 1) + 1] = {
 
 #define CAB_GET_TAG_NAME(TAG) \
     (TAG == OT_NONE) ? 0 : CAB_OUTPUT_TAG_NAMES[LOG2(TAG)]
+
+typedef struct {
+    size_t current;
+} CabOutputTagNamesIterator;  // init as {0}
+
+static inline const char* cab_output_tag_names_iter(
+    CabOutputTagNamesIterator* iterator) {
+    if (iterator->current >= LOG2(OT_END - 1)) {
+        return NULL;
+    }
+    return CAB_OUTPUT_TAG_NAMES[iterator->current++];
+}
 
 #endif
