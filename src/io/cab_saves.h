7fe4547b41af41ef43f8f035cfe27a1a419c95eb
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-09-11 15:35:54 +0200

    removed unused cab_saves.h functions

--- src/io/cab_saves.h
+++ src/io/cab_saves.h
@@ -5,17 +5,12 @@
 #include <stddef.h>
 
 #include "cab_session.h"
+#include "word.h"
 
 #define DEFAULT_VOCAB_PATH "data/words/5_letters_en_words.txt"
 #define DEFAULT_SAVES_PATH "data/saves/last.saves"
 
-bool are_save_files_valid(void);
-
 void load_saves(void);
-
-void store_saves(void);
-
-void delete_save_files(void);
 
 void cab_session__save_data(CabSession* session);
 
