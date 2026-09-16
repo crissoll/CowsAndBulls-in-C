d5d4306548116405f65472e01c2f66f66f97cea0
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-09-16 17:46:32 +0200

    fixed cab_play_turn function argument type

--- src/api/cab_api.c
+++ src/api/cab_api.c
@@ -2,7 +2,7 @@
 #include "cab_io_api.h"
 #include "cab_session_api.h"
 
-const char* cab_play_turn(char* input_string) {
+const char* cab_play_turn(const char* input_string) {
     if (cab_session__set_input(cab_get_session(), input_string) ==
         INPUT_SUCCESS) {
         cab_process_turn();
