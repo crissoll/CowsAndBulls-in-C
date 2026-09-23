d27b1eec4c58f9dc094089c96b325b41eebcdc1d
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-09-23 16:34:48 +0200

    fixed clean buffer \n bug

--- src/file_handling/cab_saves.c
+++ src/file_handling/cab_saves.c
@@ -38,6 +38,7 @@ static void clean_buffer(char* buffer) {
     for (size_t i = 0; i < len; i++) {
         if (comment) {
             if (buffer[i] == '\n') {
+                support_buffer[j++] = buffer[i];
                 comment = false;
             }
             continue;
