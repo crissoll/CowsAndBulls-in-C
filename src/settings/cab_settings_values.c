370ebe6c32b854e067e0cc1518c0917a12a180b5
ChristianumSolacium <sollazzocristiano@gmail.com>
2026-09-23 16:18:56 +0200

    added settings names and getter function

--- src/settings/cab_settings_values.c
+++ src/settings/cab_settings_values.c
@@ -19,6 +19,7 @@
 typedef bool (*SettingValueValidationFunc)(size_t value);
 
 typedef struct {
+    const char* name;
     size_t min_value;
     size_t max_value;
     size_t default_value;
@@ -31,30 +32,42 @@ bool validate_special_command_char(size_t value) {
 }
 
 static const SettingsSpec setting_specs[STG_LEN] = {
-    [STG_Display_IndexArray_WordsPerLine] = {0, 100, 10},
-    [STG_Display_RevealSecretWordOnSurrender] = {false, true, true},
-    [STG_Internal_WordLen] = {1, MAX_PRACTICAL_WORD_LEN, 5},
-    [STG_Internal_DetectWordLenFromVocab] = {false, true, true},
-    [STG_Rule_LoseOnMaxAttemptsReached] = {false, true, false},
-    [STG_Internal_MaxAttempts] = {1, MAX_PRACTICAL_ATTEMPTS,
+    [STG_Display_IndexArray_WordsPerLine] = {"words-per-line", 0, 100, 10},
+    [STG_Display_RevealSecretWordOnSurrender] =
+        {"reveal-secret-word-on-surrender", false, true, true},
+    [STG_Internal_WordLen] = {"word-len", 1, MAX_PRACTICAL_WORD_LEN, 5},
+    [STG_Internal_DetectWordLenFromVocab] = {"set-word-len-from-vocabulary",
+                                             false, true, true},
+    [STG_Rule_LoseOnMaxAttemptsReached] = {"lose-on-max-attempts-reached",
+                                           false, true, false},
+    [STG_Internal_MaxAttempts] = {"max-attempts", 1, MAX_PRACTICAL_ATTEMPTS,
                                   MAX_PRACTICAL_ATTEMPTS},
-    [STG_Display_RevealSecretWordOnAttemptsFinished] = {false, true, true},
-    [STG_Rule_VocabularyConstraintMode] = {CONSTR_None, CONSTR_LoseGame,
+    [STG_Display_RevealSecretWordOnAttemptsFinished] =
+        {"reveal-secret-word-on-attempts-finished", false, true, true},
+    [STG_Rule_VocabularyConstraintMode] = {"vocabulary-constraint-mode",
+                                           CONSTR_None, CONSTR_LoseGame,
                                            CONSTR_SkipAttempt},
-    [STG_Rule_AttemptsCoherenceConstraintMode] = {CONSTR_None, CONSTR_LoseGame,
-                                                  CONSTR_None},
-    [STG_Rule_AttemptsEqualityConstraintMode] = {CONSTR_None, CONSTR_LoseGame,
-                                                 CONSTR_SkipAttempt},
-    [STG_Internal_AllowDuplicateLetters] = {false, true, true},
-    [STG_Internal_VocabDecimationPercentage] = {0, 100, 0},
-    [STG_Internal_CommandPrefixChar] = {0, 255, 0},
-    [STG_Debug_LogMode] = {0, 256, LOG_ToFile},
-    [STG_Debug_LogMessages] = {false, true, true},
-    [STG_Display_TextWrapMaxLineLength] = {10, 1000, 80},
-    [STG_Debug_LogInput] = {false, true, true},
-    [STG_Debug_LogInputPrompt] = {false, true, true},
-    [STG_Internal_ShowPlayAgainPrompt] = {false, true, true},
-    [STG_Debug_LogVocabularyDiscardedWords] = {false, true, true},
+    [STG_Rule_AttemptsCoherenceConstraintMode] =
+        {"attempts-coherence-constraint-mode", CONSTR_None, CONSTR_LoseGame,
+         CONSTR_None},
+    [STG_Rule_AttemptsEqualityConstraintMode] =
+        {"attempts-equality-constraint-mode", CONSTR_None, CONSTR_LoseGame,
+         CONSTR_SkipAttempt},
+    [STG_Internal_AllowDuplicateLetters] = {"allow-duplicate-letters", false,
+                                            true, true},
+    [STG_Internal_VocabDecimationPercentage] =
+        {"vocabulary-decimation-percentage", 0, 100, 0},
+    [STG_Internal_CommandPrefixChar] = {"command-prefix-char", 0, 255, 0},
+    [STG_Debug_LogMode] = {"log-mode", 0, 256, LOG_ToFile},
+    [STG_Debug_LogMessages] = {"log-messages", false, true, true},
+    [STG_Display_TextWrapMaxLineLength] = {"max-output-line-length", 10, 1000,
+                                           80},
+    [STG_Debug_LogInput] = {"log-input", false, true, true},
+    [STG_Debug_LogInputPrompt] = {"log-input-prompt", false, true, true},
+    [STG_Internal_ShowPlayAgainPrompt] = {"show-play-again-prompt", false, true,
+                                          true},
+    [STG_Debug_LogVocabularyDiscardedWords] = {"log-vocabulary-discarded-words",
+                                               false, true, true},
 
 };
 
@@ -131,4 +144,8 @@ CabSettingValidity cab_settings__get_setting_value_validity(
     }
 
     return CAB_SV_Valid;
+}
+
+const char* cab_settings__get_name(CabSettingId setting) {
+    return setting_specs[setting].name;
 }
--- src/settings/cab_settings_values.h
+++ src/settings/cab_settings_values.h
@@ -18,6 +18,8 @@ typedef enum {
 
 bool cab_setting_is_initialized(CabSettingId setting);
 
+const char* cab_settings__get_name(CabSettingId setting);
+
 size_t cab_settings__get_default_value(CabSettingId setting);
 
 size_t cab_settings__get_min_value(CabSettingId setting);
